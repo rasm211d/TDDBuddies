6a121257145831200c0647ce5e5b97470a338fc9
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-09-24 15:00:19 +0200

    Test til Fibonacci.class

---
+++ test/FibonacciTest.java
@@ -0,0 +1,29 @@
+import junit.framework.TestCase;
+
+public class FibonacciTest extends TestCase {
+
+    public void testcalculateResult(){
+    long resultat;
+
+        //Test med helt tal
+        resultat = Fibonacci.calculateResult(5);
+        assertEquals(15, resultat);
+
+        //Test med negativt tal
+        resultat = Fibonacci.calculateResult(-5);
+        assertEquals(-15, resultat);
+
+        //Test med ugyldigt tal (-1 betyder fejl i programmet)
+        resultat = Fibonacci.calculateResult(2.5);
+        assertEquals(-1, resultat);
+
+        //Test med 0
+        resultat = Fibonacci.calculateResult(0);
+        assertEquals(0, resultat);
+
+        //Test med stort tal
+        resultat = Fibonacci.calculateResult(92);
+        assertEquals(7540113804746346429l, resultat);
+
+    }
+}
