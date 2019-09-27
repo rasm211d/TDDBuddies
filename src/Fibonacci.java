137a14d86634b64436bb7829ae311e40b2a49bfc
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-09-27 12:35:28 +0200

    Har skrevet kode til test

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,33 @@
 public class Fibonacci {
+
+    static int calculateResult(int n) {
+        /*long num1 = 0;
+        long num2 = 1;
+        long sum = 0;
+
+        for(long count = 1; count <= n; count++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+
+
+        }
+
+         */
+        int num1 = 0;
+        int num2 = 1;
+        System.out.println("Fibonacci Series of "+n);
+        int sumOfPrevTwo = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.println(num1+" ");
+
+            sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+        }
+        return sumOfPrevTwo;
+
+    }
+
 }
